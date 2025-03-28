043176e62940a19d6a87b93db806d90e7c275cbf
Hieu Pham <phamgiatrihieu@gmail.com>
2025-03-28 11:48:15 +0700

    init and config app

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E1E1E',
+        secondary: '#D1D5DB',
+        accent: '#6CCD76',
+        light: {
+          50: '#f0fdf2',
+          100: '#dcfce4',
+          200: '#baf7cb',
+          300: '#6CCD76', // the same accent color
+          400: '#5dbb6a',
+          500: '#4da85d',
+          600: '#3d964f',
+          700: '#2e8341',
+          800: '#1e7133',
+          900: '#0f5e25',
+        },
+        dark: {
+          50: '#ebfcee',
+          100: '#d6f9dd',
+          200: '#adf3bb',
+          300: '#6CCD76', // the same accent color
+          400: '#5cb868',
+          500: '#4da35a',
+          600: '#3d8e4c',
+          700: '#2e793e',
+          800: '#1e6430',
+          900: '#0f4f22',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
